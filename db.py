9c8168488a12c10ba4f1db511d557d27ae2ed891
Susainathan <92146999+Susainathan@users.noreply.github.com>
2024-06-26 07:00:03 +0530

    Update db.py

--- db.py
+++ db.py
@@ -4,4 +4,4 @@ con=sqlite3.connect("datatwo.db")
 print("database created")
 
 con.execute("create table user(ID integer primary key autoincrement,Name text not null,Last_Name text not null,DOB integer not null,Username varchar(40) not null,Password varchar(20) not null)")
-print("table created")
+print("table created...")
